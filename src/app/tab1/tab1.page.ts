aef3ae10105a41af664ffe5180559ea5917aad86
Natália Firmino <nataliaffirmino2401@gmail.com>
2025-10-16 21:53:18 -0300

    Conteúdo da aula sobre Backend -  dia 16/10

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,6 +8,27 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  public entrada: number = 0;
+  public saida: number = 0;
+
+  total(): number {
+    return this.entrada - this.saida;
+  }
   constructor() {}
 
+  adicionar() {
+    this.entrada++;
+  }
+
+  retirar(){
+    this.saida++;
+  }
+
+  obterCor(): string {
+    if (this.total() > 0) {
+      return 'primary';
+    }else{
+      return 'danger';
+    }
+}
 }
